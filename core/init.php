450432861582cb162320769fb45e4a9dabc15d13
Popinho96 <antonio.popic@gmail.com>
2019-02-05 20:21:31 +0100

    Added sanitize and init files

---
+++ core/init.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+session_regenerate_id();
+
+spl_autoload_register(function($class){
+    require_once 'classes/'.$class.'.php'
+});
+
+require_once 'functions/sanitize.php';
+
+?>
