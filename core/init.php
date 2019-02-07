fe8731f053ed705081f06711fb5928a4ad50bc14
Popinho96 <antonio.popic@gmail.com>
2019-02-07 18:29:49 +0100

    Added error reporting

--- core/init.php
+++ core/init.php
@@ -1,6 +1,11 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', true);
+
 session_start();
-session_regenerate_id();
+//session_regenerate_id();
 
 spl_autoload_register(function($class){
     require_once 'classes/'.$class.'.php';
